2540bc1aaca0b74b4e282fb78552310faa6157cc
jaircunhac <jaircunhacneto@gmail.com>
2024-05-05 16:19:00 -0300

    Commit inicial

---
+++ decorator/src/test/java/desgin/pattern/decorator/DecoratorApplicationTests.java
@@ -0,0 +1,13 @@
+package desgin.pattern.decorator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecoratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
